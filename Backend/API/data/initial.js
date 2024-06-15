0e526150251f46a143e724880e051bdda7d1dbba
slucarini <slucarini@medialab.com.ar>
2024-06-15 13:25:15 -0300

    Actualizando directorios

--- Backend/API/data/initial.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const data = require('./data.json');
-var conection = require('../controller/connectionDB.js');
-var model = require('../models/ArticleSchema.js');
-
-conection.conectar();
-
-model.create(data).then(() => console.log('La carga inicial tuvo éxito')).catch((err) => console.error("Error ", err)).finally(() => mongoose.connection.close());
