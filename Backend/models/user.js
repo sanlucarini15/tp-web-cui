40920679a25bda0f11f48602396af8156863c248
orsingheraxel <orsingheraxel@medialab.com.ar>
2024-08-07 19:31:47 -0300

    Agregando roles a usuarios

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
+const roles = ['ADMIN', 'USER', 'INVITED'];
+
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  preferences: { type: [String], default: [] }
+  preferences: { type: [String], default: [] },
+  role: { type: [String], enum: roles, required: true } //Puede tener mas de un ROL
 });
 
 const User = mongoose.model('User', userSchema);
